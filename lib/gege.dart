08c619a0baf4d92ac6d670767ea7073f0c8ede10
Cristy Lea Laquindanum <lea.laquindanum@fortress-asya.com>
2025-01-20 11:39:14 +0800

    another

--- lib/gege.dart
+++ lib/gege.dart
@@ -5,6 +5,8 @@ class Gege extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Text('data'),
+    );
   }
 }
